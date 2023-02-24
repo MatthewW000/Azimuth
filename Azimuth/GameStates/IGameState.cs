625a4c91a5241107e3bd3a2b29888893c047a573
Matthew Watkins <s230018@students.aie.edu.au>
2023-02-24 13:52:00 +1100

    Core Framework complete

---
+++ Azimuth/GameStates/IGameState.cs
@@ -0,0 +1,12 @@
+﻿namespace Azimuth.GameStates
+{
+	public interface IGameState
+	{
+		public string ID { get; }
+
+		public void Load();
+		public void Unload();
+		public void Update(float _deltaTime);
+		public void Draw();
+	}
+}
